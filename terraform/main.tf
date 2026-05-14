e25c8264bd1f989498468efa22c8e25eb875f76a
Praveen kumar S <praveen0031kumar@gmail.com>
2026-05-14 21:46:37 +0530

    ci/cd t2

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,39 +13,9 @@ provider "aws" {
 }
 
 # 2. Security Group (Opening ports for SSH, Frontend, and Backend)
-resource "aws_security_group" "simple_sg" {
-  name        = "simple-hashtag-sg"
-  description = "Allow SSH and App traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Backend
-  }
-
-  ingress {
-    from_port   = 5173
-    to_port     = 5173
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Frontend
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+data "aws_security_group" "existing_sg" {
+  name = "simple-hashtag-sg"
 }
-
 # 3. Single EC2 Instance
 resource "aws_instance" "app_server" {
   ami           = "ami-091138d0f0d41ff90" # Ubuntu 22.04 LTS
