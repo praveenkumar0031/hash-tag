0a240033b0fa6515a14fd37f5b917b2a2834aab8
Praveen kumar S <praveen0031kumar@gmail.com>
2026-05-14 21:25:09 +0530

    ci/cd

---
+++ terraform/main.tf
@@ -0,0 +1,86 @@
+# 1. Provider Configuration
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# 2. Security Group (Opening ports for SSH, Frontend, and Backend)
+resource "aws_security_group" "simple_sg" {
+  name        = "simple-hashtag-sg"
+  description = "Allow SSH and App traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Backend
+  }
+
+  ingress {
+    from_port   = 5173
+    to_port     = 5173
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Frontend
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 3. Single EC2 Instance
+resource "aws_instance" "app_server" {
+  ami           = "ami-091138d0f0d41ff90" # Ubuntu 22.04 LTS
+  instance_type = "t3.micro"
+  key_name      = "test" # Ensure this matches your AWS Key Pair name
+  vpc_security_group_ids = [aws_security_group.simple_sg.id]
+
+  # This script runs at first boot to setup the environment
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update -y
+              sudo apt-get install -y docker.io
+              sudo systemctl start docker
+              sudo usermod -aG docker ubuntu
+
+              # Replace 'your_username' with your actual Docker Hub username
+              docker pull praveen0031/hashtag-backend:latest
+              docker pull praveen0031/hashtag-frontend:latest
+
+              # Run the Backend
+              docker run -d --name backend -p 8000:8000 \
+                -e MONGO_URI="mongodb+srv://praveen:dbPass0031@cluster0.fqnvpdr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" \
+                praveen0031/hashtag-backend:latest
+
+              # Run the Frontend
+              docker run -d --name frontend -p 5173:80 \
+                praveen0031/hashtag-frontend:latest
+              EOF
+
+  tags = {
+    Name = "Simple-Hashtag-Deploy"
+  }
+}
+
+# 4. Output the Public IP
+output "server_public_ip" {
+  value = aws_instance.app_server.public_ip
+}
